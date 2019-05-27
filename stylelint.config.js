f7117d005a20c76cee6f5d1d8821c588d7eb3bbd
Spyros Argalias <sargalias@gmail.com>
2019-05-27 21:01:17 +0100

    feat(stylelint): Add rule for SCSS class pattern

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,5 +10,9 @@ module.exports = {
     'sh-waqar/declaration-use-variable': [['color', 'background-color']],
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': [
+      '^([A-Z][a-zA-Z0-9]*)(_[a-z0-9][a-zA-Z0-9]*)?(___[a-z0-9][a-zA-Z0-9]*)?$',
+      { resolveNestedSelectors: true },
+    ],
   },
 };

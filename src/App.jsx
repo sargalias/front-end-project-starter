3430af3448f2f6b98d115986a5a8ba6e211c6a6e
Spyros Argalias <sargalias@gmail.com>
2019-05-27 16:34:17 +0100

    feat(sample code): Add sample SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './App.scss';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-    <p>Text</p>
+  <div className={styles.App}>
+    <h1 className={styles.App_heading}>App</h1>
+    <p className={styles.App_text}>Text</p>
   </div>
 );
 

9a2d5657998d41b9497fa64563bfdc4a6bd29534
Spyros Argalias <sargalias@gmail.com>
2019-06-01 15:19:31 +0100

    feat(cleanup): Add sample test with dynamic import

--- src/foo.test.js
+++ src/foo.test.js
@@ -4,3 +4,13 @@ test('foo', async () => {
   const result = await foo();
   expect(result).toBe('foo');
 });
+
+test('foo mock', async () => {
+  jest.resetModules();
+  jest.doMock('./foo', () => ({
+    __esModule: true,
+    default: 'fooMock',
+  }));
+  const fooModule = await import('./foo');
+  expect(fooModule.default).toBe('fooMock');
+});
